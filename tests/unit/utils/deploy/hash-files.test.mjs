d84c25172e5bffe689c59997ee8ecd42fd3dd786
AJ Schmidt <aschmidt@nvidia.com>
2023-04-23 17:25:35 -0400

    WIP, add edge fn assertions to `hashFiles` unit tests

--- tests/unit/utils/deploy/hash-files.test.mjs
+++ tests/unit/utils/deploy/hash-files.test.mjs
@@ -1,3 +1,4 @@
+import { Response } from 'node-fetch'
 import { expect, test } from 'vitest'
 
 import { DEFAULT_CONCURRENT_HASH } from '../../../../src/utils/deploy/constants.mjs'
@@ -13,30 +14,35 @@ test('Hashes files in a folder', async () => {
         content: `Root page`,
       })
       .withSymlink({ target: 'public/index.html', path: 'public/file1.html' })
+      .withEdgeFunction({handler: async () => new Response('Edge Function works'), name: 'edge',})
       .buildAsync()
 
     // add an edge function file here!
     const netlifyConfigFile = 'netlify.toml'
     const regularDeployFile = 'index.html'
     const symlinkDeployFile = 'file1.html'
-    const expectedFiles = [netlifyConfigFile, regularDeployFile, symlinkDeployFile]
+    const edgeFnFile = 'edge.js'
     const { files, filesShaMap } = await hashFiles({
       concurrentHash: DEFAULT_CONCURRENT_HASH,
       configPath: `${builder.directory}/netlify.toml`,
       deployFolder: `${builder.directory}/public`,
+      edgeFunctionsFolder: `${builder.directory}/netlify/edge-functions`,
       rootDir: builder.directory,
       statusCb() {},
     })
 
-    expect(Object.entries(files)).toHaveLength(expectedFiles.length)
-    expect(Object.values(filesShaMap).flat()).toHaveLength(expectedFiles.length)
+    // 1 config file + 1 regular file + 1 symlink + 1 edge fn file = 4
+    expect(Object.entries(files)).toHaveLength(4)
+    expect(Object.values(filesShaMap).flat()).toHaveLength(4)
 
+    // config file assertions
     const configFileSha = files[netlifyConfigFile]
     const configFileShaMapEntry = filesShaMap[configFileSha]
     expect(configFileShaMapEntry.length).toBe(1)
     expect(configFileShaMapEntry[0].normalizedPath).toBe('netlify.toml')
     expect(configFileShaMapEntry[0].path).toBe(`${builder.directory}/netlify.toml`)
 
+    // regular file & symlink assertions
     const regularDeployFileSha = files[regularDeployFile]
     const symlinkDeployFileSha = files[symlinkDeployFile]
     const regularDeployFileShaMapEntry = filesShaMap[regularDeployFileSha]
@@ -51,5 +57,13 @@ test('Hashes files in a folder', async () => {
     expect(symlinkDeployFileShaMapArrayEntry.normalizedPath).toBe(symlinkDeployFile)
     expect(regularDeployFileShaMapArrayEntry.path).toBe(`${builder.directory}/public/${regularDeployFile}`)
     expect(symlinkDeployFileShaMapArrayEntry.path).toBe(`${builder.directory}/public/${symlinkDeployFile}`)
+
+    // edge fn assertions
+    const edgeFnNormalized = `.netlify/internal/edge-functions/${edgeFnFile}`
+    const edgeFnFileSha = files[edgeFnNormalized]
+    const edgeFnFileShaMapEntry = filesShaMap[edgeFnFileSha]
+    expect(edgeFnFileShaMapEntry.length).toBe(1)
+    expect(edgeFnFileShaMapEntry[0].normalizedPath).toBe(edgeFnNormalized)
+    expect(edgeFnFileShaMapEntry[0].path).toBe(`${builder.directory}/netlify/edge-functions/${edgeFnFile}`)
   })
 })
