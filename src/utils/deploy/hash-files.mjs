72763d4c818d2489e8887d0fe796d8f04eb80bef
AJ Schmidt <aschmidt@nvidia.com>
2023-04-22 20:41:28 -0400

    WIP, fix file object normalization

--- src/utils/deploy/hash-files.mjs
+++ src/utils/deploy/hash-files.mjs
@@ -1,12 +1,8 @@
-import { relative } from 'path'
-
 import fg from "fast-glob";
 import hasha from 'hasha'
 import pMap from 'p-map';
 
-import { deployFileNormalizer } from '../../lib/edge-functions/deploy.mjs'
-
-import { normalizePath } from './util.mjs'
+import { normalizeFileObj } from './util.mjs'
 
 
 const hashFiles = async ({
@@ -26,16 +22,17 @@ const hashFiles = async ({
 
   const [regularDeployFiles, wellKnownDeployFiles] = await Promise.all([
     fg(
-      [configPath, `${deployFolder}/**`, edgeFunctionsDistPath].filter(Boolean),
+      [configPath, `${deployFolder}/**`, `${edgeFunctionsDistPath}/**`].filter(Boolean),
       {
         objectMode: true,
-        ignore: ["**/__MACOSX/**", skipNodeModules ? `${rootDir}/node_modules/**` : ""].filter(Boolean)
+        ignore: ["**/__MACOSX/**", skipNodeModules ? `${rootDir}/node_modules/**` : ""].filter(Boolean),
+        absolute: true
       }
     ),
     // ".well-known" needs its own query until https://github.com/mrmlnc/fast-glob/issues/86 is resolved
     fg(
       [`${deployFolder}/**/.well-known/**`],
-      { objectMode: true, dot: true }
+      { objectMode: true, dot: true, absolute: true }
     )
   ])
 
@@ -45,22 +42,17 @@ const hashFiles = async ({
   const files = {}
   // hash: [fileObj, fileObj, fileObj]
   const filesShaMap = {}
-
   const mapper = async (fileObj) => {
-    const relname = relative(deployFolder, fileObj.path)
-    const normalizedPath = normalizePath(relname)
-    const normalizedFileObj = deployFileNormalizer(rootDir,
-      { ...fileObj, assetType: "file", normalizedPath, relname }
-    )
+    const normalizedFileObj = normalizeFileObj(fileObj, { configPath, deployFolder, edgeFunctionsFolder: edgeFunctionsDistPath })
 
     statusCb({
       type: 'hashing',
-      msg: `Hashing ${relname}`,
+      msg: `Hashing ${normalizedFileObj.normalizedPath}`,
       phase: 'progress',
     })
-    const hash = await hasha.fromFile(fileObj.path, { algorithm: hashAlgorithm })
+    const hash = await hasha.fromFile(normalizedFileObj.path, { algorithm: hashAlgorithm })
 
-    files[normalizedPath] = hash
+    files[normalizedFileObj.normalizedPath] = hash
 
     // We map a hash to multiple fileObjs because the same file
     // might live in two different locations
