94f419bb32498656d515c197ea4dcc935b4f6ff3
AJ Schmidt <aschmidt@nvidia.com>
2023-04-23 16:39:55 -0400

    WIP, update `hashFiles` unit tests

--- tests/unit/utils/deploy/hash-files.test.mjs
+++ tests/unit/utils/deploy/hash-files.test.mjs
@@ -12,27 +12,44 @@ test('Hashes files in a folder', async () => {
         path: 'public/index.html',
         content: `Root page`,
       })
+      .withSymlink({ target: 'public/index.html', path: 'public/file1.html' })
       .buildAsync()
 
-    const expectedFiles = ['netlify.toml', 'public/index.html']
+    // add an edge function file here!
+    const netlifyConfigFile = 'netlify.toml'
+    const regularDeployFile = 'index.html'
+    const symlinkDeployFile = 'file1.html'
+    const expectedFiles = [netlifyConfigFile, regularDeployFile, symlinkDeployFile]
     const { files, filesShaMap } = await hashFiles({
-      deployFolder: builder.directory,
-      configPath: `${builder.directory}/netlify.toml`,
       concurrentHash: DEFAULT_CONCURRENT_HASH,
+      configPath: `${builder.directory}/netlify.toml`,
+      deployFolder: `${builder.directory}/public`,
+      rootDir: builder.directory,
       statusCb() {},
     })
 
     expect(Object.entries(files)).toHaveLength(expectedFiles.length)
-    expect(Object.entries(filesShaMap)).toHaveLength(expectedFiles.length)
+    expect(Object.values(filesShaMap).flat()).toHaveLength(expectedFiles.length)
 
-    expectedFiles.forEach((filePath) => {
-      const sha = files[filePath]
-      expect(sha).toBeDefined()
+    const configFileSha = files[netlifyConfigFile]
+    const configFileShaMapEntry = filesShaMap[configFileSha]
+    expect(configFileShaMapEntry.length).toBe(1)
+    expect(configFileShaMapEntry[0].normalizedPath).toBe('netlify.toml')
+    expect(configFileShaMapEntry[0].path).toBe(`${builder.directory}/netlify.toml`)
 
-      const fileObjArray = filesShaMap[sha]
-      fileObjArray.forEach((fileObj) => {
-        expect(fileObj.normalizedPath).toBe(filePath)
-      })
-    })
+    const regularDeployFileSha = files[regularDeployFile]
+    const symlinkDeployFileSha = files[symlinkDeployFile]
+    const regularDeployFileShaMapEntry = filesShaMap[regularDeployFileSha]
+    expect(regularDeployFileShaMapEntry.length).toBe(2)
+    expect(regularDeployFileSha).toBe(symlinkDeployFileSha)
+
+    const regularDeployFileShaMapArrayEntry = regularDeployFileShaMapEntry.find(el => el.normalizedPath === regularDeployFile)
+    const symlinkDeployFileShaMapArrayEntry = regularDeployFileShaMapEntry.find(el => el.normalizedPath === symlinkDeployFile)
+    expect(regularDeployFileShaMapArrayEntry).toBeDefined()
+    expect(symlinkDeployFileShaMapArrayEntry).toBeDefined()
+    expect(regularDeployFileShaMapArrayEntry.normalizedPath).toBe(regularDeployFile)
+    expect(symlinkDeployFileShaMapArrayEntry.normalizedPath).toBe(symlinkDeployFile)
+    expect(regularDeployFileShaMapArrayEntry.path).toBe(`${builder.directory}/public/${regularDeployFile}`)
+    expect(symlinkDeployFileShaMapArrayEntry.path).toBe(`${builder.directory}/public/${symlinkDeployFile}`)
   })
 })
